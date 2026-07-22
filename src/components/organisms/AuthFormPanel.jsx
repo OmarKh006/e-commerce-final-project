4e1155f74779885bab6593af0a035f277c71230e
OmarKh006 <khairyomar006@gmail.com>
2026-07-23 01:56:32 +0300

    fixing login/signup img

--- src/components/organisms/AuthFormPanel.jsx
+++ src/components/organisms/AuthFormPanel.jsx
@@ -3,7 +3,7 @@ export default function AuthFormPanel({ children }) {
     <div className="flex flex-col md:flex-row items-center gap-10 py-10">
       <div className="hidden md:block flex-1">
         <img
-          src="https://picsum.photos/seed/authpanel/700/700"
+          src="/SideImage.png"
           alt=""
           className="w-full h-130 object-cover rounded-sm bg-[#CBE8F4]"
         />
